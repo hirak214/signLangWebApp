0035db4aa897a29a656b9d4f0f1139ac590dc51c
Hirak Desai <hirak214@gmail.com>
2022-10-09 00:31:00 +0530

    viewing camera using cv2

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ import time
 import mediapipe as mp
 
 # Keypoints using MP Holistic
+cap = cv2.VideoCapture(1)
+while cap.isOpened():
+    ret, frame = cap.read()
+    cv2.imshow('OpenCV Freed', frame)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
